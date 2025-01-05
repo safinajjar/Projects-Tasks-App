e25027194ead029a5d10a75ee89deb1940c02421
Musti <safi.najjar1@gmail.com>
2025-01-05 17:52:37 +0100

    display projects as ref

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,10 +1,26 @@
 <script lang="ts" setup>
-import { RouterLink } from 'vue-router'
+import supabase from '@/lib/supabaseClient'
+import { ref } from 'vue'
+
+const projects = ref()
+
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) {
+    console.error(error)
+  }
+
+  projects.value = data
+
+  console.log('projects', projects.value)
+})()
 </script>
 
 <template>
   <div>
     <h1>Projects Page</h1>
+    <pre>{{ projects }}</pre>
     <RouterLink to="/">Go to home</RouterLink>
   </div>
 </template>
