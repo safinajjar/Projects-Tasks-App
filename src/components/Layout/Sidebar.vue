aed6409224c60159fe2fbefe9bff2cbd14f2c4c7
Musti <safi.najjar1@gmail.com>
2025-01-06 13:56:30 +0100

    Sidebar

---
+++ src/components/Layout/Sidebar.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { Button } from '@/components/ui/button'
+import SidebarLinks from './SidebarLinks.vue'
+
+const links = [
+  { icon: 'lucide:house', title: 'Dashboard', to: '/' },
+  { icon: 'lucide:building-2', title: 'Projects', to: '/projects' },
+  { icon: 'lucide:badge-check', title: 'My Tasks', to: '/tasks' },
+]
+
+const accountLinks = [
+  { icon: 'lucide:user', title: 'Profile', to: '/profile' },
+  { icon: 'lucide:settings', title: 'Settings', to: '/settings' },
+  { icon: 'lucide:log-out', title: 'Sign out', to: '/signout' },
+]
+</script>
+
+<template>
+  <aside
+    class="flex flex-col h-screen gap-2 border-r fixed bg-muted/40 lg:w-52 w-16 transition-[width]"
+  >
+    <div class="flex h-16 items-center border-b px-2 lg:px-4 shrink-0 gap-1 justify-between">
+      <Button variant="outline" size="icon" class="w-8 h-8">
+        <iconify-icon icon="lucide:menu"></iconify-icon>
+      </Button>
+
+      <Button variant="outline" size="icon" class="w-8 h-8">
+        <iconify-icon icon="lucide:plus"></iconify-icon>
+      </Button>
+    </div>
+
+    <nav class="flex flex-col gap-2 justify-between h-full relative">
+      <div>
+        <SidebarLinks :links="links" />
+      </div>
+
+      <div class="border-y text-center bg-background py-3">
+        <SidebarLinks :links="accountLinks" />
+      </div>
+    </nav>
+  </aside>
+</template>
---
+++ src/components/Layout/SidebarLinks.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+interface LinkProp {
+  icon: string
+  title: string
+  to: string
+}
+
+defineProps<{
+  links: LinkProp[]
+}>()
+</script>
+
+<template>
+  <RouterLink
+    v-for="link in links"
+    :key="link.title"
+    :to="link.to"
+    exact-active-class="text-primary bg-muted"
+    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
+  >
+    <iconify-icon :icon="link.icon"></iconify-icon>
+    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+  </RouterLink>
+</template>
