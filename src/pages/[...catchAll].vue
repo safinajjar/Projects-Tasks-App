85815b1d189a6d5665d023b519b94afcc934cee3
Musti <safi.najjar1@gmail.com>
2025-01-07 10:09:43 +0100

    add app error page

--- src/pages/[...catchAll].vue
+++ src/pages/[...catchAll].vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
+import AppErrorPage from '@/components/AppError/AppErrorPage.vue'
 usePageStore().pageData.title = '404 Page not found'
 </script>
 
+<script setup lang="ts"></script>
+
 <template>
-  <div>
-    <h1>404 Page not found</h1>
-  </div>
+  <AppErrorPage />
 </template>
