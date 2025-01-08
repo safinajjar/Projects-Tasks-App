9bb3e4379d3505331bd1bae6603bdc90ad9f4482
Musti <safi.najjar1@gmail.com>
2025-01-08 13:43:03 +0100

    auth store set session upon login/register

--- src/utils/supaAuth.ts
+++ src/utils/supaAuth.ts
@@ -1,5 +1,8 @@
 import supabase from '@/lib/supabaseClient'
+import { useAuthStore } from '@/stores/auth'
 import type { LoginForm, RegisterForm } from '@/types/AuthForm'
+
+const authStore = useAuthStore()
 
 export const register = async (formData: RegisterForm) => {
   const { data, error } = await supabase.auth.signUp({
@@ -25,11 +28,13 @@ export const register = async (formData: RegisterForm) => {
     }
   }
 
+  authStore.setAuth(data.session)
+
   return true
 }
 
 export const login = async (formData: LoginForm) => {
-  const { error } = await supabase.auth.signInWithPassword({
+  const { data, error } = await supabase.auth.signInWithPassword({
     email: formData.email,
     password: formData.password
   })
@@ -38,6 +43,8 @@ export const login = async (formData: LoginForm) => {
     console.error(error)
     return
   }
+
+  authStore.setAuth(data.session)
 
   return true
 }
